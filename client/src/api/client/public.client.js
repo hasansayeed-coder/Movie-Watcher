63be44cdb738d3a60ee3c8180b57e306e07b74a4
Mahamudul Hasan <hasansayeed791@gmail.com>
2026-03-24 14:31:09 +0600

    fix: use env variable for API URL

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
+// private.client.js AND public.client.js
 const baseURL = process.env.REACT_APP_API_URL || "http://localhost:5000/api/v1";
 
 const publicClient = axios.create({
